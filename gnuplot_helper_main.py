8be451e2bf3c77144c2d25817148d2dd8e0eaf83
no-glue <nikola_pav@eunet.rs>
2015-01-22 14:10:56 +0100

    multiple files

--- gnuplot_helper_main.py
+++ gnuplot_helper_main.py
@@ -7,11 +7,11 @@ import gnuplot_helper
 if __name__ == "__main__":
   argv = sys.argv[1:]
   parser = argparse.ArgumentParser()
-  parser.add_argument("-files")
+  parser.add_argument("-files", nargs='+')
   parser.add_argument("-out")
   parser.add_argument("-skipColumns")
   parser.add_argument("-terminal")
   args = parser.parse_args(argv)
 
   plot = gnuplot_helper.GnuplotHelper()
-  plot.plot_multiple_xy(args.files.split(","), args.out, args.skipColumns, args.terminal)
+  plot.plot_multiple_xy(args.files, args.out, args.skipColumns, args.terminal)
