ad1f861fdefe0639fb56c6046e5b8ed9866c973e
no-glue <nikolapav1985@gmail.com>
2015-04-30 15:42:33 +0200

    graph labels points

--- gnuplot_helper.py
+++ gnuplot_helper.py
@@ -18,7 +18,7 @@ class GnuplotHelper(object):
             print skipped
             floats = skipped.astype("float")
             data = Gnuplot.Data(floats[:,1], floats[:,0], with_="lp", title=f)
-            data_points = Gnuplot.Data(floats[:,1], floats[:,0], using="1:2:2", with_="labels offset 0", title=f)
+            data_points = Gnuplot.Data(floats[:,1], floats[:,0], using="1:2:2", with_="labels offset 1")
             if i == 0:
                 g.plot(data)
                 g.replot(data_points)
